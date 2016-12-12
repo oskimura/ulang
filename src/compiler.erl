78ac789068877d4e98ef5cb7b4a52d213fb99398
oskimura <oskimura@gmail.com>
2016-12-12 16:25:21 +0900

    add compile function

--- src/compiler.erl
+++ src/compiler.erl
@@ -36,3 +36,45 @@ file(File) ->
         _ ->
             erlang:error("file read error")
     end.
+
+
+
+compile(File) ->
+    case file:read_file(File) of
+        {ok,Bin} ->
+            case ulang:string(binary_to_list(Bin)) of
+                {ok,Ret,_} ->
+                    io:format("~p~n",[Ret]),
+                    case ulang_yecc:parse(Ret) of
+                        {ok,Spec} ->
+                            io:format("~p~n",[Spec]),
+                            case compile:noenv_forms(Spec,[return]) of
+                                {ok,Module,Binary,Warnings} ->
+				    case file:open(Module,[write,binary]) of
+					{ok,Dev} ->
+					    io:format("binret:~p",[Binary]),
+					    file:write(Dev,Binary),
+					    file:close(Dev);
+					{error,Reason} ->
+					    erlang:error({"file error",Reason})
+				    end;
+				error ->
+                                    erlang:error({"compile errord"});
+                                {error,Error,Warning} ->
+                                    erlang:error({"compile error",Error,Warning})
+
+			    
+                                end;
+                        {error,Reason} ->
+                            erlang:error({"parse error",Reason});
+                        _ ->
+                            erlang:error("parse error")
+                    end;
+                {error,Reason,_} ->
+                    erlang:error({"lex error",Reason});
+                _ ->
+                    erlang:error("lex error")
+            end;
+        _ ->
+            erlang:error("file read error")
+    end.
