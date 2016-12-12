a2df03dacc1ec9d30ad36c1dd822c1595bdde533
oskimura <oskimura@gmail.com>
2016-12-12 17:19:11 +0900

    add interpreter

--- src/compiler.erl
+++ src/compiler.erl
@@ -78,3 +78,39 @@ compile(File) ->
         _ ->
             erlang:error("file read error")
     end.
+
+
+eval(Bin,Env) ->
+    case ulang:string(Bin) of
+                {ok,Ret,_} ->
+                    io:format("~p~n",[Ret]),
+                    case ulang_yecc:parse(Ret) of
+                        {ok,Spec} ->
+                            io:format("~p~n",[Spec]),
+			    try erl_eval:expr(Spec, Env, none,none,none) of
+				{value, Value, NewBind} ->
+				    {Value,NewBind}
+			    catch
+				Class:Exception ->
+				    erlang:error({"eval error",{Class,Exception}})
+				end;
+			{error,Reason} ->
+                            erlang:error({"parse error",Reason});
+                        _ ->
+                            erlang:error("parse error")
+                    end;
+	{error,Reason,_} ->
+	    erlang:error({"lex error",Reason});
+	_ ->
+	    erlang:error("lex error")
+    end.
+
+
+repl() ->
+    Env = erl_eval:bindings(erl_eval:new_bindings()),
+    repl(Env).
+repl(Env) ->
+    Expr = io:get_line("$ "),
+    {Val,NEnv}= eval(Expr,Env),
+    io:format("#~p~n",[Val]),
+    repl(NEnv).
