a762b1368c33984d378d0aa5dda783cf12a500c7
oskimura <oskimura@gmail.com>
2017-01-26 21:54:57 +0900

    clean code refctoring compiler:compile/1

--- src/compiler.erl
+++ src/compiler.erl
@@ -57,6 +57,16 @@ loader({Module,Binary,Warnings}) ->
             erlang:error({"load error",What}) 
     end.
 
+save_beam(Module,Binary) ->
+    case file:open(atom_to_list(Module) ++ ".beam",[write,binary]) of
+        {ok,Dev} ->
+            io:format("binret:~p",[Binary]),
+            file:write(Dev,Binary),
+            file:close(Dev);
+        {error,Reason} ->
+            erlang:error({"file error",Reason})
+    end.
+
 file(File) ->
     call_with_read_file(File,
                         fun(Bin) ->
@@ -68,45 +78,13 @@ file(File) ->
 
 
 compile(File) ->
-    case file:read_file(File) of
-        {ok,Bin} ->
-            case ulang_lex:string(binary_to_list(Bin)) of
-                {ok,Ret,_} ->
-                    io:format("~p~n",[Ret]),
-                    case ulang_yecc:parse(Ret) of
-                        {ok,Spec} ->
-                            io:format("~p~n",[Spec]),
-                            case compile:noenv_forms(Spec,[return]) of
-                                {ok,Module,Binary,Warnings} ->
-                                    case file:open(atom_to_list(Module) ++ ".beam",[write,binary]) of
-                                        {ok,Dev} ->
-                                            io:format("binret:~p",[Binary]),
-                                            file:write(Dev,Binary),
-                                            file:close(Dev);
-                                        {error,Reason} ->
-                                            erlang:error({"file error",Reason})
-                                    end;
-                                error ->
-                                    erlang:error({"compile errord"});
-                                {error,Error,Warning} ->
-                                    erlang:error({"compile error",Error,Warning})
-
-                            
-                                end;
-                        {error,Reason} ->
-                            erlang:error({"parse error",Reason});
-                        _ ->
-                            erlang:error("parse error")
-                    end;
-                {error,Reason,_} ->
-                    erlang:error({"lex error",Reason});
-                _ ->
-                    erlang:error("lex error")
-            end;
-        _ ->
-            erlang:error("file read error")
-    end.
-
+    call_with_read_file(File,
+                        fun(Bin) ->
+                                Lexed = lexer(binary_to_list(Bin)),
+                                AST = parser(Lexed),
+                                {Module,Binary,_} = compiler(AST),
+                                save_beam(Module,Binary)
+                        end).
 
 eval(Bin,Env) ->
     case ulang_lex:string(Bin) of
